8fd63005b947cf684cc4e5c6283e552d711f5156
dylancocoletzi <86747062+dylancocoletzi@users.noreply.github.com>
2022-06-28 10:51:53 -0700

    Step 2

---
+++ vaccine-hub-api/models/user.js
@@ -0,0 +1,13 @@
+const { UnauthorizedError } = require("../utils/errors")
+
+class User{
+    static async login(credentials){
+        throw new UnauthorizedError("Invalid email/password combo")
+    }
+
+    static async register(credentials){
+        
+    }
+}
+
+module.exports = User
